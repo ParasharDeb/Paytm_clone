b370c7735ee1f39a71ef5087f5aae67111d477b7
Parashar Deb <parashardeb11@gmail.com>
2025-07-11 12:20:24 +0530

    added backend to frontend

--- react_frontend/src/pages/signin.tsx
+++ react_frontend/src/pages/signin.tsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import InputBox from "../components/inputbox";
 import Buttoncomponent from "../components/Button";
 import Heading from "../components/headings";
 import BottomWarning from "../components/bottomwarning";
+import axios from "axios";
 
 export default function signin(){
     const [email, setemail] = useState("");
     const [password, setPassword] = useState("");  
+    const navigate = useNavigate();
     return(
         <div className="flex items-center justify-center h-screen bg-gray-100">
             <div className="bg-white py-2 px-8 rounded-lg shadow-md w-96 h-110">
@@ -16,7 +19,14 @@ export default function signin(){
            
             <div className="flex items-center justify-center"><Buttoncomponent children="Signup" onClickhandler={
                 async()=>{
-                    console.log("Signup clicked", email, password);
+                    axios.post("http://localhost:3000/api/v1/user/signin", {
+                        email: email,
+                        password: password
+                    }).then(()=>{
+                        navigate("/dashboard");
+                    }).catch((error)=>{
+                        console.error("Error during signin", error);
+                    });
                 }} 
                 varient="primary" size="large"/></div>
             <div><BottomWarning children="Already have an account?" buttontext="Signin" to="/signin"/></div>
--- react_frontend/src/pages/signup.tsx
+++ react_frontend/src/pages/signup.tsx
@@ -3,12 +3,14 @@ import InputBox from "../components/inputbox";
 import Buttoncomponent from "../components/Button";
 import Heading from "../components/headings";
 import BottomWarning from "../components/bottomwarning";
-
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 export default function Signup(){
     const [email, setemail] = useState("");
     const [password, setPassword] = useState("");  
     const [firstname, setfirstname] = useState("");
     const [lastname, setlastname] = useState("");
+    const navigate = useNavigate();
     return(
         <div className="flex items-center justify-center h-screen bg-gray-100">
             <div className="bg-white py-2 px-8 rounded-lg shadow-md w-96 h-150">
@@ -19,7 +21,16 @@ export default function Signup(){
             <div><InputBox type="text" label="Last Name" value={lastname} onChange={(e)=>{setlastname(e.target.value)}}/></div>            
             <div className="flex items-center justify-center"><Buttoncomponent children="Signup" onClickhandler={
                 async()=>{
-                    console.log("Signup clicked", email, password, firstname, lastname);
+                    axios.post("http://localhost:3000/api/v1/user/signup", {
+                        email: email,
+                        password: password,
+                        firstname: firstname, 
+                        lastname: lastname
+                    }).then(()=>{
+                        navigate("/dashboard");
+                    }).catch((error)=>{
+                        console.error("Error during signup", error);
+                    });
                 }} 
                 varient="primary" size="large"/></div>
             <div><BottomWarning children="Already have an account?" buttontext="Signin" to="/signin"/></div>
