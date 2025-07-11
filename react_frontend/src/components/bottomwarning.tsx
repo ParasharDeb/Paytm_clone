498ca2590e59fa75ceb652e7d525b084ec782c67
Parashar Deb <parashardeb11@gmail.com>
2025-07-11 12:09:44 +0530

    pretty signup

--- react_frontend/src/components/bottomwarning.tsx
+++ react_frontend/src/components/bottomwarning.tsx
@@ -1,7 +1,14 @@
-export default function BottomWarning(children: string) {
+import { Link } from "react-router-dom";
+
+export default function BottomWarning({children,to,buttontext}: {children:string,buttontext:string,to:string}) {
     return (
-        <div className="text-sm text-gray-500 text-center my-4">
+        <div className="flex  items-center justify-center my-4">
+        <div className="text-sm text-gray-500 text-center mx-2">
             {children}
+        </div>
+        <Link to={to} className="text-blue-500 hover:underline">
+            {buttontext}
+        </Link>
         </div>
     );
 }
--- react_frontend/src/pages/signup.tsx
+++ react_frontend/src/pages/signup.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import InputBox from "../components/inputbox";
 import Buttoncomponent from "../components/Button";
 import Heading from "../components/headings";
+import BottomWarning from "../components/bottomwarning";
 
 export default function Signup(){
     const [email, setemail] = useState("");
@@ -9,19 +10,21 @@ export default function Signup(){
     const [firstname, setfirstname] = useState("");
     const [lastname, setlastname] = useState("");
     return(
-        <div className="flex items-center justify-center h-screen">
-            <div>
+        <div className="flex items-center justify-center h-screen bg-gray-100">
+            <div className="bg-white py-2 px-8 rounded-lg shadow-md w-96 h-150">
             <Heading children='Signup'/>
             <div><InputBox type="text" label="email" value={email} onChange={(e)=>setemail(e.target.value)}/></div>            
             <div><InputBox type="password" label="Password" value={password} onChange={(e)=>{setPassword(e.target.value)}}/></div>
             <div><InputBox type="text" label="First Name" value={firstname} onChange={(e)=>{setfirstname(e.target.value)}}/></div>            
             <div><InputBox type="text" label="Last Name" value={lastname} onChange={(e)=>{setlastname(e.target.value)}}/></div>            
-            <div className="flex items-center justify-center "><Buttoncomponent children="Signup" onClickhandler={
+            <div className="flex items-center justify-center"><Buttoncomponent children="Signup" onClickhandler={
                 async()=>{
                     console.log("Signup clicked", email, password, firstname, lastname);
                 }} 
                 varient="primary" size="large"/></div>
+            <div><BottomWarning children="Already have an account?" buttontext="Signin" to="/signin"/></div>
             </div>
+            
         </div>
     )
 }
